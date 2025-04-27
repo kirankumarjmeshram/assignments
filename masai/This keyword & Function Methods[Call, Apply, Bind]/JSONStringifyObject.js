4fc51f065c5154483a33f845bde24f1620400b73
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2025-04-27 12:18:23 +0530

    * JSON Stringify Object

---
+++ masai/This keyword & Function Methods[Call, Apply, Bind]/JSONStringifyObject.js
@@ -0,0 +1,6 @@
+let student = {
+    name: "Alice",
+    age: 22,
+    course: "Computer Science"
+  };
+let jsonStr = JSON.stringify(student);//{"name":"Alice","age":22,"course":"Computer Science"}  
