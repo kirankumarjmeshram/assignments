b10d2078054745c89ff31d0b998f7061a197f0b8
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2025-05-31 22:30:09 +0530

    Basic Closure Example

---
+++ masai/closure in js/basicClosure.js
@@ -0,0 +1,13 @@
+function outerFunction() {
+  const message = "Hello from the closure!";
+
+  function innerFunction() {
+    console.log(message);
+  }
+
+  return innerFunction; 
+}
+
+const myClosure = outerFunction();
+
+myClosure(); // Hello from the closure!
