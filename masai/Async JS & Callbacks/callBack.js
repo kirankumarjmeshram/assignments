606310dce1562a58bf32beaac8a9ea9e7a00f5db
Kirankumar J Meshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2025-06-20 17:09:20 +0530

    call back function

---
+++ masai/Async JS & Callbacks/callBack.js
@@ -0,0 +1,13 @@
+function taskOne() {
+  console.log("Task 1 completed");
+}
+
+function taskTwo(callback) {
+  console.log("Task 2 completed");
+  callback(); 
+}
+
+taskTwo(taskOne);
+
+// Task 2 completed
+// Task 1 completed
